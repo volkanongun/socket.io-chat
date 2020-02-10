23e0855f427e1d72d19944e561c39b862236fabe
Volkan Ongun <me@volkanongun.com>
2020-02-10 19:15:55 +0300

    webcam added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+import cv from 'opencv4nodejs';
 import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
@@ -7,8 +7,12 @@ const app = express();
 const server = http.Server(app);
 const io = new SocketIO(server);
 
+const FPS = 10;
+const wCap = new cv.VideoCapture(0);
+wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
+
 app.use(express.static(__dirname + '/frontend'));
-app.use('/three',express.static(__dirname + '/node_modules/three/build/'));
 app.use('/io',express.static(__dirname + '/node_modules/socket.io-client/dist/'));
 
 io.on('connection', function(socket){
@@ -29,5 +33,11 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
+
+setInterval(() => {
+  const frame = wCap.read();
+  const image = cv.imencode('.jpg', frame).toString('base64');
+  io.emit('image', image)
+}, 1000 / FPS)
 
 server.listen(4000,() => console.log('Server running on port 4000'));
